2241a7eecbc7595f67d48620415c5a8f1fa5f0d5
Abdelmonom Nakhli <abdelmonom.nakhli@esprit.tn>
2024-04-12 12:47:43 +0100

    fourth commit

---
+++ src/Entity/Category.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(name: "idCategory")]
+    private ?int $id = null;
+
+    #[ORM\Column(name: "nomCategory")]
+    private ?string $nomcategory = null;
+
+    #[ORM\Column]
+    private ?string $description = null;
+
+    #[ORM\OneToMany(mappedBy: "category", targetEntity: Produit::class)]
+    private Collection $produits;
+
+    public function __construct()
+    {
+        $this->produits = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomcategory(): ?string
+    {
+        return $this->nomcategory;
+    }
+
+    public function setNomcategory(string $nomcategory): static
+    {
+        $this->nomcategory = $nomcategory;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Produit[]
+     */
+    public function getProduits(): Collection
+    {
+        return $this->produits;
+    }
+
+    public function addProduit(Produit $produit): self
+    {
+        if (!$this->produits->contains($produit)) {
+            $this->produits[] = $produit;
+            $produit->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduit(Produit $produit): self
+    {
+        if ($this->produits->removeElement($produit)) {
+            // set the owning side to null (unless already changed)
+            if ($produit->getCategory() === $this) {
+                $produit->setCategory(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Form/CommandeType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Commande;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommandeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('orderDate')
+            ->add('status')
+            ->add('modePaiement')
+            ->add('adresseLivraison')
+            ->add('fraisLivraison')
+            ->add('total')
+            ->add('idPanier')
+            ->add('userID')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Commande::class,
+        ]);
+    }
+}
